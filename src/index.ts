8b5d83751dd5b5d31ed3f8d58771377e7d8d9976
Vasya-556 <vkobrin556@gmail.com>
2025-12-18 19:47:09 +0200

    feat: add left movement

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+const body = document.body as HTMLBodyElement;
 const score_label = document.getElementById("score-label") as HTMLLabelElement
 const record_label = document.getElementById("record-label") as HTMLLabelElement
 const restart_button = document.getElementById("restart-button") as HTMLButtonElement
@@ -35,7 +36,7 @@ const draw_grid = () => {
     }
 }
 
-const draw_cube = (grid: number[][]) => {
+const draw_cubes = (grid: number[][]) => {
     const colors = {
         2: "#0000FF",
         4: "#00FF00",
@@ -51,7 +52,6 @@ const draw_cube = (grid: number[][]) => {
 
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
-            console.log(grid[i]![j])
             if (grid[i]![j] !== 0){
                 ctx.beginPath()
                 ctx.rect(16+i*76,16+j*76,60,60)
@@ -69,7 +69,49 @@ const draw_cube = (grid: number[][]) => {
 
 draw_grid()
 grid[1]![0] = 2048
+grid[0]![1] = 128
 grid[1]![1] = 128
+grid[2]![1] = 256
+grid[3]![1] = 512
 grid[1]![2] = 64
+grid[2]![2] = 128
 grid[1]![3] = 8
-draw_cube(grid)
+draw_cubes(grid)
+
+body.addEventListener("keyup", (event: KeyboardEvent) => {
+    switch(event.key) {
+        case("ArrowLeft"):
+            move_cubes_left()
+            console.log("Left")
+            break;
+        case("ArrowRight"):
+            console.log("Right")
+            break;
+        case("ArrowUp"):
+            console.log("Up")
+            break;
+        case("ArrowDown"):
+            console.log("Down")
+            break;
+    }
+})
+
+const move_cubes_left = () => {
+    for (let x = 0; x < 4; x++) {        
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (grid[i]![j]! === grid[i+1]![j]!){
+                    grid[i]![j]! += grid[i+1]![j]!
+                    grid[i+1]![j]! = 0
+                }
+                if (grid[i]![j]! === 0){
+                    grid[i]![j]! = grid[i+1]![j]!
+                    grid[i+1]![j]! = 0
+                }
+            }
+        }
+    }
+    console.log(grid)
+    draw_grid()
+    draw_cubes(grid)
+}
